35be95fe8b213b85c3dcc0d0e77901e137e29c1e
jackhr <jc2o@mac.com>
2021-05-18 20:29:37 -0400

    first draft

--- js/main.js
+++ js/main.js
@@ -2,9 +2,20 @@ const divEl = document.getElementById('main-div');
 const h1Els = [...document.querySelectorAll('#main-div h2')];
 
 divEl.addEventListener('click', handleShow);
+divEl.addEventListener('mouseover', handleExpand);
+divEl.addEventListener('mouseout', handleShrink);
+
+function handleExpand(evt) {
+  if (!h1Els.includes(evt.target)) return;
+}
+
+function handleShrink(evt) {
+  if (!h1Els.includes(evt.target)) return;
+}
 
 function handleShow(evt) {
   if (!h1Els.includes(evt.target)) return;
   let info = evt.target.nextSibling.nextSibling;
   info.className = (info.className === "hidden" ? "visible" : "hidden");
+  
 }

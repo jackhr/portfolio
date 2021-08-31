830e4a0536434d18cc5484bfcd356beaec3679e5
jackhr <jc2o@mac.com>
2021-08-30 21:28:57 -0400

    Create basic projects tabs.

--- js/main.js
+++ js/main.js
@@ -5,9 +5,46 @@ const projectsEl = document.getElementById('projects-div');
 const navAboutEl = document.getElementById('nav-about');
 const navProjectsEl = document.getElementById('nav-projects');
 const navContactEl = document.getElementById('nav-contact');
+const titleDivEl = document.getElementById('title-div');
+const ongoingEls = [...document.getElementsByClassName('ongoing')];
+const commaEls = [...document.getElementsByClassName('comma')];
+const anticipatedEls = [...document.getElementsByClassName('anticipated')];
+const projectTabEls = [...document.getElementsByClassName('title')];
 const navEls = [navAboutEl, navProjectsEl, navContactEl];
 
-navEl.addEventListener('click', (evt) => {
+navEl.addEventListener('click', handleNav);
+titleDivEl.addEventListener('click', handleProjectTabs);
+
+function handleProjectTabs(evt) {
+  if (evt.target.id === "title-div") return;
+  const text = evt.target.innerHTML;
+  const isOngoing = text[0] === "O" ? true : false;
+  anticipatedEls.forEach(elem => {
+    elem.className = isOngoing ? "hidden" : ""
+  });
+  ongoingEls.forEach(elem => 
+    elem.className = isOngoing ? "" : "hidden"
+  );
+  commaEls.forEach(elem => 
+    elem.className = isOngoing ? "" : "hidden"
+  );
+
+
+
+
+  // const text = evt.target.innerHTML;
+  // if (evt.target.id !== "title-div") {
+  //   if (text[0] === "O") {
+  //     ongoingDivEl.style.display = "inherit";
+  //     IPDivEl.style.display = "none";
+  //   } else {
+  //     ongoingDivEl.style.display = "none";
+  //     IPDivEl.style.display = "inherit";
+  //   }
+  // }
+}
+
+function handleNav (evt) {
   const text = evt.target.innerHTML;
   if (text > 10) return;
   if (text[0] === 'A') {
@@ -29,7 +66,7 @@ navEl.addEventListener('click', (evt) => {
     contactEl.style.display = 'block';
     projectsEl.style.display = 'none';
   }
-});
+}
 
 function handleActive(count) {
   navEls.forEach((el, idx) => {
